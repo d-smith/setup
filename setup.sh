0ddbd1af3c142ae9a3f2fa19a8f10a5f79aaf2ce
Ubuntu <ubuntu@ip-10-164-70-5.ec2.internal>
2013-07-08 19:19:55 +0000

    redid node install section

--- setup.sh
+++ setup.sh
@@ -2,15 +2,11 @@
 # Simple setup.sh for configuring Ubuntu 12.04 LTS EC2 instance
 # for headless setup. 
 
-# Install nvm: node-version manager
-# https://github.com/creationix/nvm
-sudo apt-get install -y git-core
-curl https://raw.github.com/creationix/nvm/master/install.sh | sh
-
-# Load nvm and install latest production node
-source $HOME/.nvm/nvm.sh
-nvm install v0.10.12
-nvm use v0.10.12
+sudo apt-get update
+sudo apt-get install -y python-software-properties python g++ make
+sudo add-apt-repository ppa:chris-lea/node.js
+sudo apt-get update
+sudo apt-get install -y nodejs
 
 # Install jshint to allow checking of JS code within emacs
 # http://jshint.com/
