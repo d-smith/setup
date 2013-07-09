b326d403c54ddeabe543bdb3839d640721c2fa1e
d-smith <doug.smith.mail@gmail.com>
2013-07-09 12:40:03 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 
 sudo apt-get update
 sudo apt-get install -y python-software-properties python g++ make
-sudo add-apt-repository ppa:chris-lea/node.js
+sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs
 
